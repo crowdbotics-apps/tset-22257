a513efdf957293cc35f126489ef78c452aff0765
Crowdbotics <team@crowdbotics.com>
2020-11-03 15:10:40 +0000

    Initial commit.

---
+++ backend/tset_22257/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tset_22257 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tset_22257.settings')
+
+application = get_wsgi_application()
---
+++ ios/tset_22257Tests/tset_22257Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface tset_22257 : XCTestCase
+
+@end
+
+@implementation tset_22257
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
